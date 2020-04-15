cf36333a1d2bc4c0391ae540d9a8952fac453109
Daryl <clarinojd@gmail.com>
2020-04-15 08:45:28 +0800

    [FET]  Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dotnet Appsettings Demo
+
+We've setup two **profiles** in the launchSettings.json. The launch settings 
+* Nonprod
+* Prod
---
+++ src/Controllers/EnvironmentController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Dotnet.Appsettings.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EnvironmentController : ControllerBase
+    {
+
+        private readonly IConfiguration _config;
+
+        public EnvironmentController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet, Route("value")]
+        public string Get(string key)
+        {
+            return _config[key];
+        }
+    }
+}
